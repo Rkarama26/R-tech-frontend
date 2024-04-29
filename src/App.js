09a048709e00e62b63dbd1dedda91a66f0a0bc7d
Rohit <rohit.karma026@gmail.com>
2024-04-30 05:24:49 +0530

    checkout page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Cart from './customer/components/Cart/Cart';
+import Checkout from './customer/components/Checkout/Checkout';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
@@ -18,9 +20,16 @@ function App() {
         {/*<Homepage/> */}
         {/* <Product/> */}
         {/* <ProductDetails/> */}
-        <Cart/>
+        {/* <Cart/> */}
 
-        
+        <div>
+          <Router>
+            <Checkout/>
+          </Router>
+        </div>
+
+
+
       </div>
 
       <Footer />
