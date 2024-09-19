a8cd4b00e8a1146cc5fd4882788aa4898ce6f2a4
Rohit <rohit.karma026@gmail.com>
2024-09-19 23:09:57 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './customer/components/navigation/Navigation';
+import Navigation from './cust omer/components/navigation/Navigation';
 import Homepage from './customer/Pages/HomePage/Homepage';
 import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
